0db1b772bd4e949c9dbee4cbb9d1d96ce7f56191
Dheeraj kumar mahto <126415291+Dheerajmahto62@users.noreply.github.com>
2025-03-16 12:45:54 +0530

    hee

--- api/index.py
+++ api/index.py
@@ -131,5 +131,5 @@ def manage_camps():
 def home():
     return jsonify({"message": "Welcome to Blood Donation API"}), 200
 
-if __name__ == '__main__':
-    app.run(debug=True, port=45171, host="0.0.0.0")
+# if __name__ == '__main__':
+#     app.run(debug=True, port=45171, host="0.0.0.0")
--- wsgi.py
+++
@@ -1,4 +0,0 @@
-from main import app  # Import your Flask app
-
-if __name__ == "__main__":
-    app.run()
